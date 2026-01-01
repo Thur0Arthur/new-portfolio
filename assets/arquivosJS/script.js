bacafed778f8f9d14a08b445b0fb981b943c192f
Arthur Francisco Silvestre <127779644+Thur0Arthur@users.noreply.github.com>
2026-01-01 12:09:29 -0300

    Rodapé finalizado.

--- assets/arquivosJS/script.js
+++ assets/arquivosJS/script.js
@@ -128,12 +128,18 @@ const modal = document.querySelector("#modalProjetos");
 const fecharModal = document.querySelector(".fecharModal");
 const abrirModalBtns = document.querySelectorAll(".abrirModal");
 
+/*MODO DESENVOLVEDOR */
+/*
+modal.style.display = "flex";
+document.body.style.overflow = "hidden";
+*/
 abrirModalBtns.forEach((btn) => {
     btn.addEventListener("click", () => {
         modal.style.display = "flex";
         document.body.style.overflow = "hidden";
     })
 });
+
 fecharModal.addEventListener("click", () => {
     modal.style.display = "none";
     document.body.style.overflowX = "hidden";
